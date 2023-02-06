aadc5977a5f0d78d332f4434e5d1558939fa21d7
R-S-9 <rafik.saakyan.2016@mail.ru>
2023-02-06 14:25:15 +0700

    add requests limit

--- tg_openai/handlers/request.py
+++ tg_openai/handlers/request.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 from loguru import logger
 from aiogram import types
@@ -6,6 +7,8 @@ from dotenv import load_dotenv
 
 from loader import dp, bot
 from tg_openai.utils.request_verification import RequestVerification
+from tg_openai.utils.request_limit import RequestLimited
+from .subscribers import number_request
 
 
 logger.add(
@@ -17,21 +20,32 @@ logger.add(
 )
 
 load_dotenv()
-number_request = 0
 
 
 class RequestAPI:
     @staticmethod
     @dp.message_handler()
     async def handle_text(message: types.Message):
-        global number_request
-        number_request += 1
+        """Главный handler"""
+        if message.from_user.username in number_request:
+            # TODO add DB sqlite3
+            openai_api_key = os.environ.get(
+                f"API_KEY_{message.from_user.username}"
+            )
+            number_request[message.from_user.username] += 1
+        else:
+            limit_false = await RequestLimited().check_user(message)
+            if limit_false is False:
+                return None
+            openai_api_key = None
+            number_request['anonymous'] += 1
 
         try:
-            response, is_image = await RequestVerification.\
+            response, is_image = await RequestVerification. \
                 checking_for_type_request(
-                    message.text
-            )
+                    message.text, openai_api_key
+                )
+
             print(
                 f'Запрос {number_request}, {datetime.datetime.now()}'
             )
@@ -48,6 +62,7 @@ class RequestAPI:
                 await bot.send_media_group(message.chat.id, media=media)
             else:
                 await message.answer(
+                    f'Запрос №{number_request[message.from_user.username]}\n'
                     f'{response}\n\n[ChatGPTRuBot Подписаться]'
                     '(https://t.me/ru_gpt_bot)',
                     parse_mode='Markdown'
