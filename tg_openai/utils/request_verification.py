ddfd1604f4e92b944f334b53f1889ecae265b406
R-S-9 <rafik.saakyan.2016@mail.ru>
2023-03-05 01:41:38 +0700

    change verification

--- tg_openai/utils/request_verification.py
+++ tg_openai/utils/request_verification.py
@@ -5,39 +5,21 @@ from tg_openai.apps.app_openai import OpenaiChatBot
 
 class RequestVerification:
     @staticmethod
-    async def checking_for_type_request(text: str, api_key: str or None):
+    async def checking_for_type_request(text: str, user_name: str):
         """
             Если в запросе есть слово 'Изображение', то вызывается
             функция где, генерируется изображения
         """
-        # TODO добавить разрешение в запрос
 
         text = text.lower()
 
-        if re.search(r'^изображени\w', text) or \
-                re.search(r'^\d изображени\w', text):
-
-            if re.search(r',', text[:14]):
-                list_text = text.split(r', ')
-            else:
-                list_text = [
-                    text[:14], text[14:]
-                ]
-            try:
-                quantity = re.findall('[0-9]', list_text[0])
-
-                if re.findall('[0-9]', list_text[0]) and api_key:
-                    quantity = int(quantity[0])
-                else:
-                    quantity = 1
-            except Exception:
-                quantity = 1
+        if re.search(r'^изображени\w', text):
             return await OpenaiChatBot().request_openai_image(
-                f"Изображение {' '.join(list_text[1:])}",
-                "1024x1024",
-                quantity,
-                api_key,
+                text,
+                '512x512',  # "1024x1024",
+                1,
+                user_name,
             )
-        return await OpenaiChatBot().request_openai_completion(
-            text, api_key
+        return await OpenaiChatBot.request_openai_completion(
+            text, user_name
         )
