19665bd24994e5afbba41af1c66879e18dd61c3f
R-S-9 <rafik.saakyan.2016@mail.ru>
2023-03-05 01:38:58 +0700

    add connection db

--- tg_openai/main.py
+++ tg_openai/main.py
@@ -4,6 +4,8 @@ from handlers import dp
 from utils.notify_admins import BotAdministration
 from utils.set_bot_commands import set_default_commands
 
+from db.sqlite_base import SQLConnect
+
 
 async def bot_on_startup(disp: Dispatcher):
     await BotAdministration.on_startup_notify(disp)
@@ -13,4 +15,16 @@ async def bot_on_startup(disp: Dispatcher):
 
 
 if __name__ == '__main__':
+    # Отображение информации о БД
+    SQLConnect().db_information()
+
+    # Создание таблицы User
+    SQLConnect().create_user_table()
+
+    # Создание таблицы Token
+    SQLConnect().create_key_table()
+
     executor.start_polling(dp, on_startup=bot_on_startup)
+
+    # Закрытие бд
+    SQLConnect().close_sql_connection()
